c83be32ffba5505555ac444ff9a30b3ada71e33d
Josh Frechette <josh.f.design@gmail.com>
2019-11-21 06:19:39 -0500

    CLI established, octokit/rest get successful, file creation set up.

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Developer_Profile_Generator
 University of Toronto SCS Coding Bootcamp - Unit 09: Node.js and ES6+ Homework
+
+# Summary
+
+The Developer Profile Generator creates a command-line application that dynamically generates a PDF profile from a GitHub username. The user will be prompted for a favorite color, which will be used as the background color for cards.
+
+# Deployment
+
+https://github.com/TwistedPixels/Developer_Profile_Generator.git
+
+https://twistedpixels.github.io/Developer_Profile_Generator/
+
+# Usage
+
--- index.js
+++ index.js
@@ -1,11 +1,99 @@
-const questions = [
-  
-];
+const fs = require("fs");
+const inquirer = require("inquirer");
+const Octokit = require("@octokit/rest"); 
+const octokit = new Octokit();
+const generate = require("./generateHTML.js");
+const request = require('request');//npm i request
 
-function writeToFile(fileName, data) {
- 
+
+
+//for me
+// fs
+//     .readFile("generateHTML.js", "utf8", function(error, data) {
+//     if (error) {
+//         return console.log(error);
+//     }
+//     console.log(data)
+// //Get data from the user
+// })
+inquirer
+    .prompt([
+        {
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "name"
+        },
+        {
+        type: "list",
+        message: "What is your favourite color?",//Options; green, blue, red, pink.
+        name: "color",
+        choices: [
+            'green',
+            'red',
+            'blue',
+            'pink'
+        ]
+        }
+    ])
+//
+    .then(function(response) 
+    {
+        var filename = response.name.toLowerCase().split(' ').join('') + ".pdf";//Creates the specific GitHub user pdf
+        fs.writeFile(filename, JSON.stringify(response, null, '\t'), function(err) {
+    // console.log(JSON.stringify(response, null, '  '));
+    });
+
+let ghname = response.name;//To be fed into function getting a response from the 
+let ghcolor = response.color;//To be fed in generateHTML.js
+
+// console.log(ghname + " and " + ghcolor)
+options =  {
+    url: "https://api.github.com/users/" + ghname,
+    headers: {
+        "User-Agent": "TwistedPixels"  // Your Github ID or application name, auth# 
+    }
 }
 
-function init() {
+request.get(options)
+    .on('response', function (response) {
+        // console.log(response.statusCode);
+        console.log(JSON.stringify(response));
+    });
 
-init();
+// Compare: https://developer.github.com/v3/repos/#list-organization-repositories
+// octokit.repos   
+//     .listForUser({
+//         org: "",
+//         type: "public"
+//       })
+//       .then(({ data }) => {
+//         // handle data
+//       });
+// Octokit
+//     .get()
+
+// //user data I need to fill out the card
+// // profile Image
+// response.avatar_url();
+// // username
+// response.login();
+// // links toString; user location via googlemaps, user github profile, user blog
+// response.location();
+// response.html_url();
+// response.blog();
+// // user bio 
+// response.bio();
+// // #public repos 
+// response.public_repos();
+// // #GitHub Stars
+// response.starred_url();
+// // #users following
+// response.following();
+
+// function writeToFile(fileName, data) {//Create individual .pdf filenames according to the user's name (GitHub username)?
+//  generate
+//}
+
+// function init() {
+})
+// init();

25db8d8ecfeafc94799b5a004502c5dcd330d566
Josh Frechette <josh.f.design@gmail.com>
2019-11-23 10:21:53 -0500

    Functional Axios call with some HTML build.

--- index.js
+++ index.js
@@ -1,99 +1,96 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
-const Octokit = require("@octokit/rest"); 
+const Octokit = require("@octokit/rest");
 const octokit = new Octokit();
 const generate = require("./generateHTML.js");
 const request = require('request');//npm i request
+const axios = require("axios");//npm i axios
+const util = require("util");//npm i util
+var pdfMaker = require('pdf-maker'); //npm i --save pdf-maker ==> might not be the best library
+const GenHTML = require("./generateHTML");
 
 
 
 //for me
-// fs
-//     .readFile("generateHTML.js", "utf8", function(error, data) {
-//     if (error) {
-//         return console.log(error);
-//     }
-//     console.log(data)
-// //Get data from the user
-// })
-inquirer
-    .prompt([
-        {
-        type: "input",
-        message: "What is your GitHub username?",
-        name: "name"
-        },
-        {
-        type: "list",
-        message: "What is your favourite color?",//Options; green, blue, red, pink.
-        name: "color",
-        choices: [
-            'green',
-            'red',
-            'blue',
-            'pink'
-        ]
+fs
+    .readFile("generateHTML.js", "utf8", function (error, data) {// HTML file being read into the function
+        if (error) {
+            return console.log(error);
         }
-    ])
-//
-    .then(function(response) 
-    {
-        var filename = response.name.toLowerCase().split(' ').join('') + ".pdf";//Creates the specific GitHub user pdf
-        fs.writeFile(filename, JSON.stringify(response, null, '\t'), function(err) {
-    // console.log(JSON.stringify(response, null, '  '));
-    });
+        // console.log(data)
+        //Get data from the user
+        inquirer
+            .prompt([
+                {
+                    type: "input",
+                    message: "What is your GitHub username?",
+                    name: "name"
+                },
+                {
+                    type: "list",
+                    message: "What is your favourite color?",//Options; green, blue, red, pink.
+                    name: "color",
+                    choices: [
+                        'green',
+                        'red',
+                        'blue',
+                        'pink'
+                    ]
+                }
+            ])
 
-let ghname = response.name;//To be fed into function getting a response from the 
-let ghcolor = response.color;//To be fed in generateHTML.js
+            //
+            .then(function (response) //skip this to continue collecting data, save write function for the writefile method towards the end?
 
-// console.log(ghname + " and " + ghcolor)
-options =  {
-    url: "https://api.github.com/users/" + ghname,
-    headers: {
-        "User-Agent": "TwistedPixels"  // Your Github ID or application name, auth# 
-    }
-}
+            {
+                // var filename = response.name.toLowerCase().split(' ').join('') + ".pdf";//Creates the specific GitHub user pdf filename
+                // fs.writeFile("user_files/" + filename, JSON.stringify(response, null, '\t'), function (err) {//Creates the specific GitHub user pdf, trying to add the folder name, see if that works to organize the cards
+                //     // console.log(JSON.stringify(response, null, '  '));
+                // })
 
-request.get(options)
-    .on('response', function (response) {
-        // console.log(response.statusCode);
-        console.log(JSON.stringify(response));
-    });
+                let ghname = response.name;//To be fed into function getting a response from the 
+                let ghcolor = response.color;//To be fed in generateHTML.js
+                console.log(ghname + " and " + ghcolor)
 
-// Compare: https://developer.github.com/v3/repos/#list-organization-repositories
-// octokit.repos   
-//     .listForUser({
-//         org: "",
-//         type: "public"
-//       })
-//       .then(({ data }) => {
-//         // handle data
-//       });
-// Octokit
-//     .get()
+               
 
-// //user data I need to fill out the card
-// // profile Image
-// response.avatar_url();
-// // username
-// response.login();
-// // links toString; user location via googlemaps, user github profile, user blog
-// response.location();
-// response.html_url();
-// response.blog();
-// // user bio 
-// response.bio();
-// // #public repos 
-// response.public_repos();
-// // #GitHub Stars
-// response.starred_url();
-// // #users following
-// response.following();
+//function to get the GitHub users info goes here
+                const queryURL = 'https://api.github.com/users/' + ghname;
+                console.log(queryURL);
+                axios
+                    .get(queryURL).then(function (response) {
+                        const userInfo = [
+                            "profileImg: " + response.data.avatar_url,
+                            "userName: " + response.data.login,
+                            "location: " + response.data.location,
+                            "blog: " + response.data.blog,
+                            "bio: " + response.data.bio,
+                            "repos: " + response.data.public_repos,
+                            "stars: " + response.data.starred_url,
+                            "following: " + response.data.following,
+                            "-".repeat(60)
+                        ].join("\n\n");
 
-// function writeToFile(fileName, data) {//Create individual .pdf filenames according to the user's name (GitHub username)?
-//  generate
-//}
+                    console.log("data" + data)
+                    console.log("userInfo" + userInfo)
 
-// function init() {
-})
-// init();
+                
+// function here to feed the ghcolor into generateHTML.js
+
+ 
+
+    
+//===============================================================================
+
+                    // function writeToFile(fileName, data) {//Create individual .pdf filenames according to the user's name (GitHub username)?
+                    //     //  generate
+                    // }
+
+                    // function init() {
+                    // }
+                })
+
+            }
+            )})
+// init()
+

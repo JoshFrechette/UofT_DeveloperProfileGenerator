8308ef0ba48b336edfcb9e83199e7dbfdc994ed5
Josh Frechette <josh.f.design@gmail.com>
2019-11-24 23:06:20 -0500

    Values passing through to generateHTML.js, file converted to PDF.

--- index.js
+++ index.js
@@ -9,15 +9,11 @@ const util = require("util");//npm i util
 var pdfMaker = require('pdf-maker'); //npm i --save pdf-maker ==> might not be the best library
 const GenHTML = require("./generateHTML");
 
-
-
-//for me
 fs
     .readFile("generateHTML.js", "utf8", function (error, data) {// HTML file being read into the function
         if (error) {
             return console.log(error);
         }
-        // console.log(data)
         //Get data from the user
         inquirer
             .prompt([
@@ -39,58 +35,90 @@ fs
                 }
             ])
 
-            //
-            .then(function (response) //skip this to continue collecting data, save write function for the writefile method towards the end?
+            .then(function (userData) {
+                let ghname = userData.name;//To be fed into function getting a response from the 
+                // let ghcolor = response.color;//To be fed in generateHTML.js
 
-            {
-                // var filename = response.name.toLowerCase().split(' ').join('') + ".pdf";//Creates the specific GitHub user pdf filename
-                // fs.writeFile("user_files/" + filename, JSON.stringify(response, null, '\t'), function (err) {//Creates the specific GitHub user pdf, trying to add the folder name, see if that works to organize the cards
-                //     // console.log(JSON.stringify(response, null, '  '));
-                // })
-
-                let ghname = response.name;//To be fed into function getting a response from the 
-                let ghcolor = response.color;//To be fed in generateHTML.js
-                console.log(ghname + " and " + ghcolor)
-
-               
-
-//function to get the GitHub users info goes here
-                const queryURL = 'https://api.github.com/users/' + ghname;
+                //function to get the GitHub users info goes here
+                let queryURL = 'https://api.github.com/users/' + ghname;
                 console.log(queryURL);
                 axios
                     .get(queryURL).then(function (response) {
-                        const userInfo = [
-                            "profileImg: " + response.data.avatar_url,
-                            "userName: " + response.data.login,
-                            "location: " + response.data.location,
-                            "blog: " + response.data.blog,
-                            "bio: " + response.data.bio,
-                            "repos: " + response.data.public_repos,
-                            "stars: " + response.data.starred_url,
-                            "following: " + response.data.following,
-                            "-".repeat(60)
-                        ].join("\n\n");
+                        const userInfo = {
+                            "profileImg":  response.data.avatar_url,
+                            "userName":  response.data.login,
+                            "location": "https://www.google.com/maps/search/?api=" + response.data.location,
+                            "blog": response.data.blog,
+                            "bio": response.data.bio,
+                            "repos": response.data.public_repos,
+                            "followers": response.data.followers,
+                            "stars": response.data.starred_url,
+                            "following": response.data.following,
+                            "ghcolor": userData.color
+                            
+                        }
+                        console.log("userInfo" + userInfo)
 
-                    console.log("data" + data)
-                    console.log("userInfo" + userInfo)
+                        
 
-                
-// function here to feed the ghcolor into generateHTML.js
 
+
+
+
+                        // writeToFile(); //Having all the info gathered, proceed to write send the info to generateHTML.js
+                        // function here to feed the ghcolor into generateHTML.js
+                        //===============================================================================
+
+
+                        fs.writeFile("./template.html",generate(userInfo),function(err){
+                            if((err))
+                            {
+
+                                console.log(err)
+                                
+                            }
+                        })
+                                           
+
+
+
+                        var template = './template.html';
+                        var pdfPath = `./user-files/${ghname}.pdf`;
+                        var option = {
+
+                            paperSize: {
+                                format: 'A4',
+                                orientation: 'portrait',
+                                border: '1.8cm'
+                            }
+
+                        };
  
+pdfMaker(template, pdfPath, option);
+                    })
+            // function writeToFile(fileName, data) {//Create individual .pdf filenames according to the user's name (GitHub username)?   
+         
+    //  generate
 
-    
-//===============================================================================
 
-                    // function writeToFile(fileName, data) {//Create individual .pdf filenames according to the user's name (GitHub username)?
-                    //     //  generate
-                    // }
-
-                    // function init() {
-                    // }
+    // fs
+    //     .writeFile(filename, JSON.stringify(response, null, '\t'), function (err) {//Creates the specific GitHub user pdf, trying to add the folder name, see if that works to organize the cards
+    //         console.log(JSON.stringify(response, null, '  '),
+    //             console.log(fileName)
+    //         );
+    //     })
                 })
+    
 
-            }
-            )})
+
+
+
+
+})
+
+
+
+//function init() {
+//}
+
 // init()
-
